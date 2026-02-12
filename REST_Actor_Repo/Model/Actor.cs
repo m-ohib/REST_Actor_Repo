29d3fd310fff8052f8d5e66e5d8ed8915303b12b
m-ohib <mohammed_amiri@hotmail.dk>
2026-02-12 13:29:40 +0100

    Add project files.

---
+++ REST_Actor_Repo/Model/Actor.cs
@@ -0,0 +1,13 @@
+﻿namespace REST_Actor_Repo.Model
+{
+    public class Actor
+    {
+        public string? Name  {get;set;}
+        public int Id { get; set; }
+        public string? BirthYear { get; set; }
+
+        override public string ToString() { return $"Actor: {Name}, Id: {Id}, BirthYear: {BirthYear}"; }
+
+
+    }
+}
